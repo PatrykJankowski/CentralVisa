fad244d12c2f650bed1cbc46f166f4d1ba35c6af
Patryk Jankowski <patryk.jankowski@protonmail.com>
2022-07-30 15:07:42 +0200

    hp updated

--- src/front-page.php
+++ src/front-page.php
@@ -1,7 +1,36 @@
 <?php get_header(); ?>
 
-<section>
+<section class="pb-40">
+    <div class="container">
+        <div class="grid grid-cols-12 gap-[30px]">
+            <div class="col-span-12 md:col-span-5 flex flex-col justify-center">
+                <h1 class="text-primary text-5xl font-bold m-0">Check requirements for visa around the globe</h1>
+                <p class="text-xl mt-6 mb-6">Subscribe, fill up the questionnaire and get your personalised visa eligibility report</p>
+                <a class="button">Check your visa</a>
+            </div>
+            <div class="col-span-12 md:col-span-7">
+                <img src="/wp-content/themes/centralvisa/img/hero.webp" alt="Central Visa" class="max-w-full">
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="text-white bg-primary pt-40 pb-40">
+    <div class="container">
+        <h2 class="text-4xl font-bold mt-0 mb-40">Central Visa is a new solution in approaching process of applying for Visa</h2>
+
+        <div class="grid grid-cols-3 gap-[30px]">
+            <div>
+                Legal questionary estimating eligibility for Visa based on your life circumstances
+            </div>
+            <div>
+                Legal questionary estimating eligibility for Visa based on your life circumstances
+            </div>
+            <div>
+                Legal questionary estimating eligibility for Visa based on your life circumstances
+            </div>
+        </div>
+    </div>
 </section>
 
 <?php get_footer(); ?>
