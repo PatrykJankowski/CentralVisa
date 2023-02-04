f884b638d764417bebbe9f34685cc3084305d535
Patryk Jankowski <patryk.jankowski@protonmail.com>
2023-02-04 20:28:01 +0100

    Dynamic fields added to specialists page

--- src/page-specialists.php
+++ src/page-specialists.php
@@ -2,12 +2,13 @@
 
 <?php get_header(); ?>
 
+<?php $section_1 = get_field('section_1'); ?>
 <section class="pt-20 pb-20 lg:pb-40">
     <div class="container">
         <div class="grid grid-cols-12 gap-[30px]">
             <div class="col-span-12 lg:col-span-9">
-                <h1 class="text-3xl lg:text-4xl font-bold m-0">Central Visa is in partnership with various prominent agencies and experts from around the world</h1>
-                <p class="font-light mt-6 mb-20">We wrok with people that specialize in immigration law, real estate, moving services, financial advising, educational consultation, and employment services that can offer flexible solutions and help ease your immigration process.</p>
+                <h1 class="text-3xl lg:text-4xl font-bold m-0"><?php echo $section_1['title']; ?></h1>
+                <p class="font-light mt-6 mb-20"><?php echo $section_1['description']; ?></p>
             </div>
         </div>
         <div class="grid grid-cols-12 gap-[30px] mb-20">
@@ -16,11 +17,18 @@
             </div>
             <div class="col-span-12 lg:col-span-1"></div>
             <div class="col-span-12 lg:col-span-6 flex flex-col justify-center">
-                <div class="text-[84px] text-[#EEEEEE]">01</div>
-                <h2 class="text-xl lg:text-2xl font-bold mb-4">Immigration Consultants</h2>
+                <div class="text-[84px] text-[#EEEEEE]"><?php echo $section_1['number_1']; ?></div>
+                <h2 class="text-xl lg:text-2xl font-bold mb-4"><?php echo $section_1['title_1']; ?></h2>
                 <div>
-                    <p><strong>Central Visa provides law firms from each country specializing in immigration law that can support you in all kinds of visa procedures, rights of residency, obtaining citizenship, and migration through foreign investments.</strong></p>
-                    <p>We pride ourselves in choosing firms and individuals that can provide you an all-inclusive spectrum of their services.</p>
+                    <?php echo $section_1['description_1']; ?>
+                    <?php $link = $section_1['link_1'];
+                    if($link):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button button--black mt-8" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -30,11 +38,18 @@
             </div>
             <div class="col-span-12 lg:col-span-1 lg:order-2"></div>
             <div class="col-span-12 lg:col-span-6 flex flex-col justify-center lg:order-1">
-                <div class="text-[84px] text-[#EEEEEE]">02</div>
-                <h2 class="text-xl lg:text-2xl font-bold text-primary mb-4">Estate Agents</h2>
+                <div class="text-[84px] text-[#EEEEEE]"><?php echo $section_1['number_2']; ?></div>
+                <h2 class="text-xl lg:text-2xl font-bold mb-4"><?php echo $section_1['title_2']; ?></h2>
                 <div>
-                    <p><strong>We have teams of qualified real estate agents and experts available to every owner and prospective buyers for all real estate matters in your desired country of destination.</strong></p>
-                    <p>They will accompany you throughout the stages of the process of selling and buying real estate.</p>
+                    <?php echo $section_1['description_2']; ?>
+                    <?php $link = $section_1['link_2'];
+                    if($link):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button button--black mt-8" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -44,11 +59,18 @@
             </div>
             <div class="col-span-12 lg:col-span-1"></div>
             <div class="col-span-12 lg:col-span-6 flex flex-col justify-center">
-                <div class="text-[84px] text-[#EEEEEE]">03</div>
-                <h2 class="text-xl lg:text-2xl font-bold text-primary mb-4">Personalised report about your eligibility for visa</h2>
+                <div class="text-[84px] text-[#EEEEEE]"><?php echo $section_1['number_3']; ?></div>
+                <h2 class="text-xl lg:text-2xl font-bold mb-4"><?php echo $section_1['title_3']; ?></h2>
                 <div>
-                    <p><strong>We have teams of qualified real estate agents and experts available to every owner and prospective buyers for all real estate matters in your desired country of destination.</strong></p>
-                    <p>They will accompany you throughout the stages of the process of selling and buying real estate.</p>
+                    <?php echo $section_1['description_3']; ?>
+                    <?php $link = $section_1['link_3'];
+                    if($link):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button button--black mt-8" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -58,11 +80,18 @@
             </div>
             <div class="col-span-12 lg:col-span-1 lg:order-2"></div>
             <div class="col-span-12 lg:col-span-6 flex flex-col justify-center lg:order-1">
-                <div class="text-[84px] text-[#EEEEEE]">04</div>
-                <h2 class="text-xl lg:text-2xl font-bold text-primary mb-4">The visa programs Central Visa offers:</h2>
+                <div class="text-[84px] text-[#EEEEEE]"><?php echo $section_1['number_4']; ?></div>
+                <h2 class="text-xl lg:text-2xl font-bold mb-4"><?php echo $section_1['title_4']; ?></h2>
                 <div>
-                    <p><strong>We have teams of qualified real estate agents and experts available to every owner and prospective buyers for all real estate matters in your desired country of destination.</strong></p>
-                    <p>They will accompany you throughout the stages of the process of selling and buying real estate.</p>
+                    <?php echo $section_1['description_4']; ?>
+                    <?php $link = $section_1['link_4'];
+                    if($link):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button button--black mt-8" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -72,11 +101,18 @@
             </div>
             <div class="col-span-12 lg:col-span-1"></div>
             <div class="col-span-12 lg:col-span-6 flex flex-col justify-center">
-                <div class="text-[84px] text-[#EEEEEE]">05</div>
-                <h2 class="text-xl lg:text-2xl font-bold text-primary mb-4">Personalised report about your eligibility for visa</h2>
+            <div class="text-[84px] text-[#EEEEEE]"><?php echo $section_1['number_5']; ?></div>
+                <h2 class="text-xl lg:text-2xl font-bold mb-4"><?php echo $section_1['title_5']; ?></h2>
                 <div>
-                    <p><strong>We have teams of qualified real estate agents and experts available to every owner and prospective buyers for all real estate matters in your desired country of destination.</strong></p>
-                    <p>They will accompany you throughout the stages of the process of selling and buying real estate.</p>
+                    <?php echo $section_1['description_5']; ?>
+                    <?php $link = $section_1['link_5'];
+                    if($link):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button button--black mt-8" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -86,25 +122,33 @@
             </div>
             <div class="col-span-12 lg:col-span-1 lg:order-2"></div>
             <div class="col-span-12 lg:col-span-6 flex flex-col justify-center lg:order-1">
-                <div class="text-[84px] text-[#EEEEEE]">06</div>
-                <h2 class="text-xl lg:text-2xl font-bold text-primary mb-4">The visa programs Central Visa offers:</h2>
+            <div class="text-[84px] text-[#EEEEEE]"><?php echo $section_1['number_6']; ?></div>
+                <h2 class="text-xl lg:text-2xl font-bold mb-4"><?php echo $section_1['title_6']; ?></h2>
                 <div>
-                    <p><strong>We have teams of qualified real estate agents and experts available to every owner and prospective buyers for all real estate matters in your desired country of destination.</strong></p>
-                    <p>They will accompany you throughout the stages of the process of selling and buying real estate.</p>
+                    <?php echo $section_1['description_6']; ?>
+                    <?php $link = $section_1['link_6'];
+                    if($link):
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="button button--black mt-8" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
+
+<?php $section_2 = get_field('section_2'); ?>
 <section class="pt-20 lg:pt-40 pb-20 lg:pb-40 bg-grey">
     <div class="container">
         <div class="grid grid-cols-12 gap-[30px]">
             <div class="col-span-full">
-            <h1 class="text-2xl lg:text-3xl text-primary">Collaborate with Central Visa as an expert</h1>
+                <h1 class="text-2xl lg:text-3xl text-primary"><?php echo $section_2['title']; ?></h1>
                 <div>
-                    <p><strong>Central Visa offers an innovative approach to the people around the globe.</strong></p>
-                    <p>Partnering with us will bring exposure and promotion to your services and will give flexible solutions and accessibility to our clients at the same time.</p>
+                    <?php echo $section_2['description']; ?>
                 </div>
             </div>
         </div>
